7ad6a63726a35e492d315b82d99c1305b6a34664
Ruslan Bondar <mailofbondik@gmail.com>
2019-11-13 12:01:55 +0200

    youtube-logo_added

--- client/src/components/Youtube/Youtube.js
+++ client/src/components/Youtube/Youtube.js
@@ -4,6 +4,8 @@ import './Youtube.scss';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const Youtube = () => {
     const settings = {
@@ -17,36 +19,42 @@ const Youtube = () => {
     return (
         <Slider className="yt-slider" {...settings}>
             <div className="yt-slider-element">
+                <i className="fab fa-youtube"></i>
                 <div className="yt-slider-text-block">
                     <p className="yt-slider-text">Підтримки членам Організації</p>
                 </div>
             </div>
             
             <div className="yt-slider-element yt-slider-element--second">
+                <i className="fab fa-youtube"></i>
                 <div className="yt-slider-text-block">
                     <p className="yt-slider-text">Підтримки членам Організації</p>
                 </div>
             </div>
             
             <div className="yt-slider-element yt-slider-element--third">
+                <i className="fab fa-youtube"></i>
                 <div className="yt-slider-text-block">
                     <p className="yt-slider-text">Підтримки членам Організації</p>
                 </div>
             </div>
 
             <div className="yt-slider-element yt-slider-element--fourth">
+                <i className="fab fa-youtube"></i>
                 <div className="yt-slider-text-block">
                     <p className="yt-slider-text">Підтримки членам Організації</p>
                 </div>
             </div>
             
             <div className="yt-slider-element yt-slider-element--fifth">
+                <i className="fab fa-youtube"></i>
                 <div className="yt-slider-text-block">
                     <p className="yt-slider-text">Підтримки членам Організації</p>
                 </div>
             </div>
             
             <div className="yt-slider-element yt-slider-element--sixth">
+                <i className="fab fa-youtube"></i>
                 <div className="yt-slider-text-block">
                     <p className="yt-slider-text">Підтримки членам Організації</p>
                 </div>
