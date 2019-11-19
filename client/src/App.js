01f269266cedd423dac45be523a39b7cfe4219db
Andrii Cherniavskyi <andruhach07@gmail.com>
2019-11-19 14:39:51 +0200

    fix developer ac/14:39

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,15 @@ import { bindActionCreators } from "redux";
 import fetchDataAction from './fetchData';
 import { BrowserRouter, Route } from "react-router-dom";
 import Admin from './components/admin'
-<<<<<<< HEAD
-import Test from "./test";
-<<<<<<< HEAD
-import SliderSection from "./components/SliderSection/SliderSection"
-import OurProjects from './components/OurProjects';
-=======
-import Header from './components/header/Header'
-import './commonStyles/commonStyles.scss'
-import MainInfo from './components/mainInfo/MainInfo'
-import Tasks from './components/tasks/Tasks';
-import JoinModal from './components/joinModal/joinModal'
-import WriteToUsModal from './components/writeToUsModal/writeToUsModal'
-import CharContribModal from './components/charContribModal/charContribModal'
->>>>>>> Header
-=======
 import Header from "./components/header/Header.js";
 import MainInfo from "./components/mainInfo/MainInfo";
+import Tasks from './components/tasks/Tasks';
 import SliderSection from "./components/SliderSection/SliderSection"
 import OurProjects from './components/OurProjects';
 import Login from './components/login/';
 import News from './components/News/News';
 import Youtube from './components/Youtube/Youtube';
 import Spinner from './components/spinner'
->>>>>>> c6f3d8d68e2433071b97b6188d4c3a370c8db7df
 
 class App extends React.Component {
 
@@ -67,41 +52,13 @@ class App extends React.Component {
               <div className="background-for-modal" >
                 <Header/>
                 <MainInfo/>
+                <Tasks/>
               </div>
             </div>
             <News news={this.props.news} />
             <Youtube />
             <SliderSection sliderState={this.state.sliders}/>
             <div className="wrapper">
-<<<<<<< HEAD
-            <OurProjects statistic={this.props.statistic} map={map} team={teem}/>
-        <Route exact={true} path='/login' render={()=>
-            <Index update={this.update}/>
-        }/>
-        <Route exact={true} path='/admin' render={()=>
-            <Admin />
-        }/>
-        <Route exact={true} path='/' render={()=>
-            <div className="container-main">
-              <div className="background-for-modal" >
-                <Header/>
-                <MainInfo/>
-                {/* <WriteToUsModal/> */}
-                {/* <CharContribModal/> */}
-                <Tasks/>
-              </div>
-
-              
-              {/* <Test/>
-              {
-                tasks.map(({id, text, img}) => {
-                  return <div key={id}>
-                    <img alt='pic' src={img}/>
-                    <p>{text}</p>
-                  </div>
-                })
-              } */}
-=======
               <OurProjects statistic={this.props.statistic} map={map} team={teem}/>
             </div>
           </>
