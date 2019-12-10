8e5e726d6ce381102064123736c75aac76830eeb
xxx44552 <amfABxPU>
2019-12-10 22:00:44 +0200

    dropDown

--- client/src/components/dropdown/dropdown.js
+++ client/src/components/dropdown/dropdown.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './dropdownStyles.scss';
+import useFeedBack from "../../customHooks/useFeedback";
 
 const DropDown = (props) => {
+
+
+    const [status, setStatus] = useState(false);
+
+    const {
+        onChangeUserPhone,
+        userPhone} = useFeedBack();
+
+
+    function sendMail(e) {
+        e.preventDefault();
+        const form = {
+            phone: userPhone
+        };
+        fetch('/mail', {
+            method: 'post',
+            body: JSON.stringify(form),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            setStatus(true);
+            setTimeout(()=> {
+                props.callBackOnClose();
+            }, 1000);
+        })
+    }
+
     return(
         <div className="dropdown-inner">
-            <div className="dropdown-border">
+            <form onSubmit={(e) => sendMail(e)} className="dropdown-border">
                 <p className="call-back">Зворотній дзвінок</p>
                 <p className="client-number">Ваш номер телефону</p>
                 <div className="actions">
-                    <input className="input" placeholder="0679663223" />
-                    <button className="confirm-number" onClick={props.callBackOnClose}></button>
+                    {status ?
+                        <p>Отправка...</p>
+                        :
+                        <>
+                            <input onChange={onChangeUserPhone} className="input" placeholder="0679663223" />
+                            <button className="confirm-number"></button>
+                        </>
+                    }
                 </div>
-            </div>
+            </form>
         </div>
     )
 };
